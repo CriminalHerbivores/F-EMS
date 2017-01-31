1be9d94f9955acd3efb01d7ceb219391b6e2d7ff
KJH <mint_star_@naver.com>
2017-01-31 19:43:16 +0900

    commit&push>> hotfix by kjh

--- F-EMS/src/main/java/com/uni/fems/dao/impl/ManageDAOImpl.java
+++ F-EMS/src/main/java/com/uni/fems/dao/impl/ManageDAOImpl.java
@@ -3,6 +3,7 @@ package com.uni.fems.dao.impl;
 import com.ibatis.sqlmap.client.SqlMapClient;
 
 public class ManageDAOImpl {
+	
 	private SqlMapClient client;
 	public void setClient(SqlMapClient client){
 		this.client=client;
