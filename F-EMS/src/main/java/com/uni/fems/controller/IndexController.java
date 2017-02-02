1a52b30622f341ea9f0f83f704c32465be0d1816
jar <piyi@daum.net>
2017-02-02 10:00:06 +0900

    commit by jar :: security

---
+++ F-EMS/src/main/java/com/uni/fems/controller/IndexController.java
@@ -0,0 +1,43 @@
+package com.uni.fems.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String in(){
+		String url = "redirect:index";
+		return url;
+	}
+	
+	@RequestMapping("/index")
+	public String index(){
+		String url = "index";
+		return url;
+	}
+
+	@RequestMapping("/a")
+	public String a(){
+		String url = "index";
+		return url;
+	}
+	
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		String url = "redirect:/index";
+		session.invalidate();
+		return url;
+	}
+
+	@RequestMapping("/loginForm")
+	public String loginForm(HttpServletRequest request,HttpSession session) {
+		String url = "member/loginForm";	
+		return url;
+	}
+
+}
