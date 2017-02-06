1d0deace78da5412ebe342385fd0423533feb5f6
rlawl <rlawl@DESKTOP-PTBTLH3>
2017-02-07 08:52:04 +0900

    kjs

--- F-EMS/src/main/java/com/uni/fems/controller/ProfsrController.java
+++ F-EMS/src/main/java/com/uni/fems/controller/ProfsrController.java
@@ -1,5 +1,18 @@
 package com.uni.fems.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.uni.fems.service.ProfsrService;
+
+@Controller
+@RequestMapping("/profsr")
 public class ProfsrController {
+	
+	@Autowired
+	private ProfsrService profsrService;
+	
+	
 
 }
