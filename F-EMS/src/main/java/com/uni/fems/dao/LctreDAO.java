4d831d0d2c7e8a335ab139ec015088e291d3e40c
KJH <mint_star_@naver.com>
2017-02-07 21:25:30 +0900

    commit>> kjh by kjh

--- F-EMS/src/main/java/com/uni/fems/dao/LctreDAO.java
+++ F-EMS/src/main/java/com/uni/fems/dao/LctreDAO.java
@@ -4,17 +4,23 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.uni.fems.dto.LctreVO;
+import com.uni.fems.dto.Lctre_SearchVO;
 
 
 public interface LctreDAO {
 
-	public LctreVO getLctre(int lc_lctre_No) throws SQLException;	// 강의 하나 가져오기
-	public ArrayList<LctreVO> listCodeLctre(String lc_Lctre_Code) throws SQLException; // 강의 코드별 강의 리스트 가져오기
+//	Lctre_SearchVO getLctre(String lc_Lctre_No) throws SQLException;	// 강의 하나 가져오기
+//	ArrayList<Lctre_SearchVO> listComboLctre(int lc_Lctre_No) throws SQLException; // 강의 콤보박스별 강의 리스트 가져오기
+//	ArrayList<Lctre_SearchVO> listDetailLctre(int lc_Lctre_No) throws SQLException; // 강의 상세 검색
 	
-	public int insertLctre(LctreVO lctreVO) throws SQLException;	//강의 등록
-	public int updateLctre(LctreVO lctreVO) throws SQLException;	//강의 수정
-	public ArrayList<LctreVO> listLctre(int new_lctre_No) throws SQLException; // 전체 강의 목록
-	int confirmNO(int new_lctre_No) throws SQLException;
+	ArrayList<LctreVO> listLctre(Lctre_SearchVO lctre_SearchVO) throws SQLException; // 전체 강의 목록 
+
+//	int selectCount(LctreVO lctreVO) throws SQLException;	//[수강신청] 현재 학기의 전체 강의 갯수
+//	
+//	
+//	int insertLctre(LctreVO lctreVO) throws SQLException;	//강의 등록
+//	int updateLctre(LctreVO lctreVO) throws SQLException;	//강의 수정
+//	int deleteLctre(int lc_Lctre_No) throws SQLException;	//강의 삭제
 	
 }
 
--- F-EMS/src/main/java/com/uni/fems/dao/impl/LctreDAOImpl.java
+++ F-EMS/src/main/java/com/uni/fems/dao/impl/LctreDAOImpl.java
@@ -6,97 +6,144 @@ import java.util.ArrayList;
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.uni.fems.dao.LctreDAO;
 import com.uni.fems.dto.LctreVO;
+import com.uni.fems.dto.Lctre_SearchVO;
 
 public class LctreDAOImpl implements LctreDAO {
 
 	private SqlMapClient client;
+	private static LctreDAOImpl instance = new LctreDAOImpl();
+	
+	private LctreDAOImpl() {}
 	public void setClient(SqlMapClient client){
 		this.client=client;
 	}
 	
-	ArrayList<LctreVO> listForCodeLctre=null;
-	
-	
-
-	/**
-	 * 강의등록시 강의번호 중복 체크
-	 */
-	@Override
-	public int confirmNO(int new_lctre_No) throws SQLException {
-		int result = -1;
-		LctreVO lctre = (LctreVO) client.queryForObject("confirmNO", new_lctre_No);
-		if (lctre != null) {
-			result = 1;
-		} else {
-			result = -1;
-		}
-		return result;
-	}
-
-	/**
-	 * 강의 코드별 강의 가져오기
-	 */
-	@Override
-	public ArrayList<LctreVO> listCodeLctre(String lc_Lctre_Code)
-			throws SQLException {
-		ArrayList<LctreVO> listCodeLctre=new ArrayList<LctreVO>();
-		for(LctreVO lctre:listForCodeLctre){
-			if(lctre.getLc_Lctre_Code().equals(lc_Lctre_Code)){
-				listForCodeLctre.add(lctre);
-			}
-		}
-		return listForCodeLctre;
-	}
-
-	/**
-	 * 한개의 강의 정보 가져오기
-	 */
-	@Override
-	public LctreVO getLctre(int lc_Lctre_No) throws SQLException {
-		LctreVO lctre = (LctreVO) client.queryForObject("getLctre", lc_Lctre_No);
-		return lctre;
-	}
-
-
-	/**
-	 * 강의 등록
-	 */
-	@Override
-	public int insertLctre(LctreVO lctreVO) throws SQLException {
-		int result = -1;
-		if (client.insert("insertLctre", lctreVO) != null) {
-			result=-1;
-		} else {
-			result=1;
-		}
-		return result;
-	}
-
-
-	/**
-	 * 강의 수정
-	 */
-	@Override
-	public int updateLctre(LctreVO lctreVO) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public static LctreDAO getInstance() {
+		return instance;
 	}
 	
+	ArrayList<Lctre_SearchVO> listForComboLctre=null;
+	
+	
 	/**
-	 * 
+	 * 한개의 강의 정보 가져오기 (완료)
+	 */
+//	@Override
+//	public Lctre_SearchVO getLctre(String lc_Lctre_No) throws SQLException {
+//		Lctre_SearchVO lctre_SearchVO = (Lctre_SearchVO) client.queryForObject("getLctre",lc_Lctre_No);
+//		return lctre_SearchVO;
+//	}
+
+
+	/**
+	 * 강의 콤보박스별 강의 리스트 가져오기......?
+	 */
+//	@Override
+//	public ArrayList<Lctre_SearchVO> listComboLctre(int lc_Lctre_No) throws SQLException {
+//		ArrayList<Lctre_SearchVO> listComboLctre=new ArrayList<Lctre_SearchVO>();
+//		for(Lctre_SearchVO lctre_Search:listForComboLctre){
+//			if(lctre_Search.getLc_Lctre_Code().equals(lc_Lctre_No)){
+//				listComboLctre.add(lctre_Search);
+//			}
+//		}
+//		return listComboLctre;
+//	}
+	
+	
+	
+	
+	/**
+	 * 강의 상세 검색 (모름)
+	 */
+//	@Override
+//	public ArrayList<Lctre_SearchVO> listDetailLctre(int lc_Lctre_No) throws SQLException {
+//		
+//		ArrayList<Lctre_SearchVO> lctre_SearchVO = new ArrayList<Lctre_SearchVO>();
+//		String lc_Lctre_No1=lc_Lctre_No+""; //형변환
+//		
+//		if (lc_Lctre_No1.equals("")) {
+//			lc_Lctre_No1 = "%";
+//		}
+//		
+//		lctre_SearchVO=(ArrayList<Lctre_SearchVO>)client.queryForList("listDetailLctre",lc_Lctre_No1);
+//		return lctre_SearchVO;
+//		
+//	}
+	
+	/**
+	 * 전체 강의 목록 <-안쓰는
+	 */
+//	@Override
+//	public ArrayList<Lctre_SearchVO> listLctre(String lu_lctre_no)
+//			throws SQLException {
+//		ArrayList<Lctre_SearchVO> lctre_SearchVO = new ArrayList<Lctre_SearchVO>();
+//
+//		if (lu_lctre_no.equals("")) {
+//			lu_lctre_no = "%";
+//		}
+//		
+//		lctre_SearchVO=(ArrayList<Lctre_SearchVO>)client.queryForList("listProduct",lu_lctre_nm);
+//		return lctre_SearchVO;
+//	}
+	
+	
+	/**
+	 * //[수강신청] 현재 학기의 전체 강의 갯수
+	 */
+//	@Override
+//	public int selectCount(LctreVO lctreVO) throws SQLException {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+
+	/**
+	 * 전체 강의 목록
 	 */
 	@Override
-	public ArrayList<LctreVO> listLctre(int lc_Lctre_No) throws SQLException {
-		String lc_Lctre_No1=lc_Lctre_No+""; //형변환
+	public ArrayList<LctreVO> listLctre(Lctre_SearchVO lctre_SearchVO) throws SQLException {
+		ArrayList<LctreVO> listLctre = null;
 		
-		if (lc_Lctre_No1 == "") { 
-			lc_Lctre_No1= "%";
-		}
-		ArrayList<LctreVO> lctreList =(ArrayList<LctreVO>)client.queryForList("listLctre",lc_Lctre_No1);		
-		return lctreList;
-		
-		
+		listLctre = (ArrayList<LctreVO>) client.queryForList("listLctre", lctre_SearchVO);
+		return listLctre;
 	}
+
+
+	/**
+	 * 강의 등록 (완료)
+	 */
+//	@Override
+//	public int insertLctre(LctreVO lctreVO) throws SQLException {
+//		int result = -1;
+//		if (client.insert("insertLctre", lctreVO) != null) {
+//			result=-1;
+//		} else {
+//			result=1;
+//		}
+//		return result;
+//	}
+
+
+	/**
+	 * 강의 수정 (완료)
+	 */
+//	@Override
+//	public int updateLctre(LctreVO lctreVO) throws SQLException {
+//		int result = (Integer)client.update("updateLctre",lctreVO);
+//		return result;
+//	}
+	
+	
+	/**
+	 * 강의 삭제 (완료)
+	 */
+//	@Override
+//	public int deleteLctre(int lc_Lctre_No) throws SQLException {
+//		int result = (Integer)client.update("deleteLctre",lc_Lctre_No);
+//		return result;
+//	}
+
+	
+
 
 
 	
