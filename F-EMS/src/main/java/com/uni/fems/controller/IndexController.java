b565e0701484d4c019cd974192f191eaaeb4b523
KJH <mint_star_@naver.com>
2017-02-16 19:01:03 +0900

    commit&push>> kjh by kjh

--- F-EMS/src/main/java/com/uni/fems/controller/IndexController.java
+++ F-EMS/src/main/java/com/uni/fems/controller/IndexController.java
@@ -15,31 +15,31 @@ import com.uni.fems.service.ManageService;
 
 @Controller
 public class IndexController {
-	
+
 	@Autowired
 	private ManageService manageService;
-	
+
 	// css 예시
 	@RequestMapping("/cssExample")
-	public String cssExample(){
+	public String cssExample() {
 		String url = "/cssExample";
 		return url;
 	}
-	
-	///////////////////////////////////////////////////////////////////////
-	
+
+	// /////////////////////////////////////////////////////////////////////
+
 	@RequestMapping("/")
-	public String in(){
+	public String in() {
 		String url = "redirect:index";
 		return url;
 	}
-	
-	
+
 	/**
 	 * <pre>
 	 * 첫 화면으로 이동
 	 * DB에 저장된 MANAGE 테이블의 내용을 가져온다
 	 * </pre>
+	 * 
 	 * <pre>
 	 * @param model
 	 * @param session
@@ -47,33 +47,41 @@ public class IndexController {
 	 * </pre>
 	 */
 	@RequestMapping("/index")
-	public String index(Model model, HttpSession session){
-		String url = "/index";	
+	public String index(Model model, HttpSession session) {
+		String url;
 		
-		/*ManageVO manageVO=null;
-		
+		ManageVO manageVO = null;
+
 		try {
-			manageVO= manageService.getManage();
+			/*manageVO = manageService.getManage();
+
+			switch (Integer.parseInt(manageVO.getMng_Layout_Knd())) {
+			case 1: url = "/layout_type/loginLoginType";
+				break;
+			case 2: url = "/layout_type/loginBoardType";
+				break;
+			case 3: url = "/layout_type/loginMenuType";
+				break;
+			case 4:	 url = "/layout_type/loginMultiType";
+				break;
+			default: url = "/index";
+			
+			}*/
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		session.setAttribute("manageVO", manageVO);*/
-		
+
+		// session.setAttribute("manageVO", manageVO);
+
 		return url;
 	}
 
-	
-	
-	
-	
-	
-	/*@RequestMapping("/a")
-	public String a(){
-		String url = "index";
-		return url;
-	}*/
-	
+	/*
+	 * @RequestMapping("/a") public String a(){ String url = "index"; return
+	 * url; }
+	 */
+
 	@RequestMapping("/logout")
 	public String logout(HttpSession session) {
 		String url = "redirect:/index";
@@ -82,17 +90,19 @@ public class IndexController {
 	}
 
 	@RequestMapping("/loginForm")
-	public String loginForm(HttpServletRequest request,HttpSession session) {
-		String url = "member/loginForm";	
+	public String loginForm(HttpServletRequest request, HttpSession session) {
+		String url = "member/loginForm";
 		return url;
 	}
 
-	// 관리자 /////////////////////////////////////////////////////////////////////////
-	
+	// 관리자
+	// /////////////////////////////////////////////////////////////////////////
+
 	/**
 	 * <pre>
 	 * 관리자 가입
 	 * </pre>
+	 * 
 	 * <pre>
 	 * @param request
 	 * @param session
@@ -100,9 +110,9 @@ public class IndexController {
 	 * </pre>
 	 */
 	@RequestMapping("/adminJoin")
-	public String adminJoinForm(HttpServletRequest request,HttpSession session) {
-		String url = "admin/admin_management/adminJoin";	
+	public String adminJoinForm(HttpServletRequest request, HttpSession session) {
+		String url = "admin/admin_management/adminJoin";
 		return url;
 	}
-	
+
 }
